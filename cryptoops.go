8cb0a51d11c6a0ff45a94330cd0c82d3c1623781
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-14 11:55:31 +0900

    Setting up a rough library structure

---
+++ cryptoops.go
@@ -0,0 +1,36 @@
+package macaronsign
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/sha512"
+)
+
+func (s Signer) encrypt(data []byte, nonce []byte) (encrypted []byte) {
+	if s.encV == 1 {
+		b, _ := aes.NewCipher(s.encKey[:])
+		c, _ := cipher.NewGCM(b)
+		return c.Seal(nil, nonce, data, nil)
+	}
+	return
+}
+
+func (s Signer) decrypt(data []byte, nonce []byte) (decrypted []byte, err error) {
+	if s.encV == 1 {
+		b, _ := aes.NewCipher(s.encKey[:])
+		c, _ := cipher.NewGCM(b)
+		decrypted, err = c.Open(nil, nonce, data, nil)
+	}
+	return
+}
+
+func (s Signer) gensig(data []byte) (sig []byte) {
+	if s.signV == 1 {
+		hash := sha512.Sum512(data)
+		mac := hmac.New(sha512.New384, s.signKey[:])
+		mac.Write(hash[:])
+		sig = mac.Sum(nil)
+	}
+	return
+}
---
+++ macaron.go
@@ -0,0 +1,42 @@
+package macaronsign
+
+import "golang.org/x/crypto/sha3"
+
+//Signer : Signature generator
+type Signer struct {
+	EXP     int64
+	encKey  [32]byte
+	signKey [32]byte
+	encV    int
+	signV   int
+}
+
+type header struct {
+	Version           int  `json:"version"`
+	Encrypted         bool `json:"e"`
+	EncryptionVersion int  `json:"ev"`
+}
+
+type payload struct {
+	Body    string `json:"body"`
+	BodyLen int    `json:"len"`
+}
+
+//NewSigner : make new signer object
+func NewSigner(Expiry int64, key []byte, EncVersion int, SignVersion int) Signer {
+	digest := sha3.Sum512(key)
+	digest = sha3.Sum512(digest[:])
+	digest = sha3.Sum512(digest[:])
+	digest = sha3.Sum512(digest[:])
+	digest = sha3.Sum512(digest[:])
+	var encK, signK [32]byte
+	copy(encK[:], digest[:32])
+	copy(signK[:], digest[32:])
+	return Signer{
+		EXP:     Expiry,
+		encKey:  encK,
+		signKey: signK,
+		encV:    EncVersion,
+		signV:   SignVersion,
+	}
+}
